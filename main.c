40e5ed918d3caa150ac7a590c4931133566bae8a
theredguy.chas21@gmail.com <theredguy.chas21@gmail.com>
2025-05-20 17:59:39 +0200

    Dice Rolls

--- main.c
+++ main.c
@@ -5,9 +5,15 @@ int main()
 {
     // The Line below must *NOT* be changed or test will fail!
     srand(1);
-
+    int dice_rolls[100];
+    int dice_roll = (rand() % 6) + 1;
+    for (int i = 0; i < 100; i++){
+        dice_rolls[i] = (rand() % 6) + 1;
+        printf("%d\n", dice_rolls[i]);
    
     // Your code goes here
 
+
+    }
     return 0;
 }
